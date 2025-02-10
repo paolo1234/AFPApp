fc775d531d4823e4368b4ba89900e3d9a16a5ae2
Paolo Calamia <paolocalamia02@gmail.com>
2025-02-10 12:11:04 +0100

    Initial Commit: HomePage e ContentView

---
+++ AFPapp/AFPapp/AFPappApp.swift
@@ -0,0 +1,17 @@
+//
+//  AFPappApp.swift
+//  AFPapp
+//
+//  Created by Paolo Calamia on 10/02/25.
+//
+
+import SwiftUI
+
+@main
+struct AFPappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AFPapp/AFPappTests/AFPappTests.swift
@@ -0,0 +1,17 @@
+//
+//  AFPappTests.swift
+//  AFPappTests
+//
+//  Created by Paolo Calamia on 10/02/25.
+//
+
+import Testing
+@testable import AFPapp
+
+struct AFPappTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
