7b4f34420de0b29e65fab4aa6ed85463554d1057
AlessioAccardo <alessiohrdo@gmail.com>
2025-02-12 01:40:48 +0100

    Completamento quiz view su risposte corrette o errate

--- AFPapp/AFPapp/view/QuizView.swift
+++ AFPapp/AFPapp/view/QuizView.swift
@@ -13,10 +13,10 @@ struct QuizView: View {
     @State var starsNum: Int = 400
     @State var questionText: String = "What does the following code do?"
     @State var blankSpace: String = "Lorem ipsum ___ sit amet qiwfonweqfo qwajhiqwoinid fiojiqiodnqwo"
-    @State var option1: String = "Option 1"
-    @State var option2: String = "Option 2"
-    @State var option3: String = "Option 3"
-    @State var option4: String = "Option 4"
+    @State private var selectedAnswer: String? = nil
+    let answers: [String] = ["Opzione1", "Opzione2", "Opzione3", "Opzione4"]
+    let isCorrect: [Bool] = [true, false, false, false]
+    
     
     var body: some View {
         
@@ -65,31 +65,44 @@ struct QuizView: View {
                     .foregroundStyle(Color(red: 0.973, green: 0.973, blue: 1))
                     .multilineTextAlignment(.leading)
                     .padding()
-                    
-                    Text("\(blankSpace)")
-                        .font(.title2)
-                        .frame(width: 350, height: 200, alignment: .center)
-                        .multilineTextAlignment(.center)
-                        .background(.white)
-                        .cornerRadius(10)
-                        .padding()
+                
+                Text("\(blankSpace)")
+                    .font(.title2)
+                    .frame(width: 350, height: 200, alignment: .center)
+                    .multilineTextAlignment(.center)
+                    .background(.white)
+                    .cornerRadius(10)
+                    .padding()
             }
             .frame(height: 300)
             .background(LinearGradient(colors: [Color(red: 0.984, green: 0.639, blue: 0.239), Color(red: 1, green: 0.255, blue: 0.161)], startPoint: .topLeading, endPoint: .bottomTrailing))
             .cornerRadius(10)
             .padding(.bottom)
             
-            OptionButton(option: $option1)
-            OptionButton(option: $option2)
-            OptionButton(option: $option3)
-            OptionButton(option: $option4)
+            ForEach(Array(zip(answers, isCorrect)), id: \.0) { answer, correct in
+                Button(action: {
+                    selectedAnswer = answer
+                }) {
+                    Text(answer)
+                        .font(.title2)
+                        .fontWeight(.bold)
+                        .foregroundStyle(getTextColor(answer: answer))
+                        .frame(width: 350, height: 50)
+                        .background(getButtonColor(answer: answer, correct: correct))
+                        .cornerRadius(10)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color(red: 0.984, green: 0.639, blue: 0.239), lineWidth: 3)
+                        )
+                }
+                .padding(.bottom, 10)
+            }
             
             HStack {
                 Button(action: {}) {
                     Image(systemName: "arrow.backward.circle.fill")
                         .font(.system(size: 40, weight: .medium))
                         .foregroundStyle(Color(red: 0.984, green: 0.639, blue: 0.239))
-                        //.offset(x: 0, y: -10)
                 }
                 
                 Spacer()
@@ -98,37 +111,29 @@ struct QuizView: View {
                     Image(systemName: "arrow.right.circle.fill")
                         .font(.system(size: 40, weight: .medium))
                         .foregroundStyle(Color(red: 0.984, green: 0.639, blue: 0.239))
-                        //.offset(x: 0, y: -10)
                 }
             }
             .padding()
         }
     }
+    
+    func getButtonColor(answer: String, correct: Bool) -> Color {
+            if selectedAnswer == nil {
+                return Color.white
+            } else {
+                return selectedAnswer == answer ? (correct ? Color.green : Color.red) : Color.white
+            }
+        }
+    
+    func getTextColor(answer: String) -> Color {
+            return selectedAnswer == answer ? .white : .black
+        }
 }
 
 #Preview {
     QuizView()
 }
 
-struct OptionButton: View {
-    
-    @Binding var option: String
-    
-    var body: some View {
-        Button(action: {}) {
-            Text("\(option)")
-                .font(.title2)
-                .fontWeight(.bold)
-                .foregroundStyle(.black)
-        }
-        .frame(width: 350, height: 50)
-        .background(.white)
-        .cornerRadius(10)
-        .overlay(
-            RoundedRectangle(cornerRadius: 10)
-                .stroke(Color(red: 0.984, green: 0.639, blue: 0.239), lineWidth: 3)
-        )
-        .padding(.bottom, 10)
 
-    }
-}
+
+
