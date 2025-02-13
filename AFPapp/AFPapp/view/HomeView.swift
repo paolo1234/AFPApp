21548526ef5d6b6f838151b951f66df872bb5e84
Matt <mattmo1505gmail.com>
2025-02-13 10:04:40 +0100

    Difficulty bolt, menù estensibile

--- AFPapp/AFPapp/view/HomeView.swift
+++ AFPapp/AFPapp/view/HomeView.swift
@@ -3,16 +3,41 @@ import SwiftUI
 struct HomeView: View {
     
     @State var progress: Double = 0.5
-    
+    @State var theoryProgress: Double = 0.3
+    @State var quizProgress: Double = 0.7
     var body: some View {
         NavigationStack {
             List {
-                Section(title: "DATA TYPES", subTitle: "Difficulty: EASY", progress: $progress)
-                Section(title: "OPERATORS", subTitle: "Difficulty: EASY", progress: $progress)
-                Section(title: "CONTROL FLOW", subTitle: "Difficulty: EASY", progress: $progress)
-                Section(title: "STRINGS", subTitle: "Difficulty: EASY", progress: $progress)
-                Section(title: "FUNCTIONS", subTitle: "Difficulty: EASY", progress: $progress)
-                Section(title: "STRUCTURES", subTitle: "Difficulty: EASY", progress: $progress)
+                Collapsible_main(
+                    label: Section(title: "DATA TYPES", subTitle: "bolt.fill", difficulty: 1, progress: $progress),
+                content: SubSection(theoryProgress: $theoryProgress, quizProgress: $quizProgress)
+                )
+                
+                Collapsible_main(
+                    label: Section(title: "OPERATORS", subTitle: "bolt.fill", difficulty: 1, progress: $progress),
+                content: SubSection(theoryProgress: $theoryProgress, quizProgress: $quizProgress)
+                )
+                
+                Collapsible_main(
+                    label: Section(title: "CONTROL FLOW", subTitle: "bolt.fill", difficulty: 1, progress: $progress),
+                    content: SubSection(theoryProgress: $theoryProgress, quizProgress: $quizProgress)
+                )
+                
+                Collapsible_main(
+                    label: Section(title: "STRINGS", subTitle: "bolt.fill", difficulty: 1, progress: $progress),
+                    content: SubSection(theoryProgress: $theoryProgress, quizProgress: $quizProgress)
+                )
+                
+                Collapsible_main(
+                    label: Section(title: "FUNCTIONS", subTitle: "bolt.fill", difficulty: 1, progress: $progress),
+                    content: SubSection(theoryProgress: $theoryProgress, quizProgress: $quizProgress)
+                )
+                
+                Collapsible_main(
+                    label: Section(title: "STRUCTURES", subTitle: "bolt.fill", difficulty: 1, progress: $progress),
+                    content: SubSection(theoryProgress: $theoryProgress, quizProgress: $quizProgress)
+                )
+                
             }
             .listRowSpacing(20)
             .scrollContentBackground(.hidden)
@@ -29,6 +54,7 @@ struct RectangleHome: View {
     
     var title: String
     var subTitle: String
+    var difficulty: Int = 1
     
     var body: some View {
         VStack(alignment: .leading, spacing: 5) {
@@ -41,12 +67,12 @@ struct RectangleHome: View {
                 .lineLimit(nil)
                 .fixedSize(horizontal: false, vertical: true)
             
-            Text(subTitle)
-                //.foregroundColor(Color(red: 0.125, green: 0.302, blue: 0.392))
-                .foregroundColor(.white)
-                .font(.system(size: 15, weight: .light))
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .multilineTextAlignment(.leading)
+            HStack{
+                ForEach (1...difficulty, id: \.self) {_ in
+                    
+                    Image(systemName:subTitle).foregroundColor(Color.white)
+                }
+            }.padding(.top, 12)
         }
     }
 }
@@ -110,13 +136,14 @@ struct CircularProgressView: View {
 struct Section: View {
     var title: String
     var subTitle: String
+    var difficulty: Int = 1
     
     @Binding var progress: Double
     
     var body: some View {
         VStack {
             HStack(alignment: .center) {
-                RectangleHome(title: title, subTitle: subTitle)
+                RectangleHome(title: title, subTitle: subTitle, difficulty: difficulty)
                     .padding(.trailing)
                 
                 Spacer()
@@ -131,13 +158,87 @@ struct Section: View {
             .overlay(RoundedRectangle(cornerRadius: 10)
                 .stroke(LinearGradient(gradient: Gradient(colors: [Color(red: 0.941, green: 0.318, blue: 0.22), Color(red: 0.941, green: 0.318, blue: 0.22).opacity(0.5)]), startPoint: .leading, endPoint: .trailing)))
       
+ 
+    }
+}
+struct SubSection: View {
+    @Binding var theoryProgress : Double
+    @Binding var quizProgress : Double
+    var body: some View {
         
-
         
+        VStack(spacing: 20) {
+            
+            HStack{
+                VStack{
+                    PercentageHome(progress: $quizProgress)
+                        .frame(width: .none, height: 80, alignment: .trailing)
+                    
+                    Button(
+                        
+                        action:{},
+                        label:{ Text("Quiz")    }
+                        
+                    )
+                    .buttonStyle(PlainButtonStyle())
+                }
+                
+                Spacer().frame(width: 90)
+                
+                VStack{
+                    PercentageHome(progress: $theoryProgress)
+                        .frame(width: .none, height: 80, alignment: .trailing)
+                    Button(
+                        
+                    action:{},
+                    label:{ Text("Theory")  }
+                    
+                    ).buttonStyle(PlainButtonStyle())
+                }
+                
+                
+            }
+            .foregroundColor(.white)
+            .font(.system(size: 23, weight: .medium))
+            .frame(maxWidth: 390, alignment: .center)
+            .multilineTextAlignment(.center)
+            
+        }.background(LinearGradient(gradient: Gradient(colors: [Color(red: 0.984, green: 0.639, blue: 0.239), Color(red: 1, green: 0.255, blue: 0.161)]), startPoint: .trailing, endPoint: .leading))
+            .cornerRadius(40)
+            .overlay(RoundedRectangle(cornerRadius: 40)
+                .stroke(LinearGradient(gradient: Gradient(colors: [Color(red: 0.941, green: 0.318, blue: 0.22), Color(red: 0.941, green: 0.318, blue: 0.22).opacity(0.5)]), startPoint: .leading, endPoint: .trailing)))
+                
     }
 }
 
-
+struct Collapsible_main: View {
+    @State var label: Section
+    @State var content: SubSection
+    
+    @State var collapsed: Bool = true
+    var body: some View {
+        VStack {
+            Button(
+                action: { self.collapsed.toggle()},
+                label: {
+                        self.label
+                }
+            )
+            .buttonStyle(PlainButtonStyle())
+            
+            Spacer().frame(height:0)
+            
+            VStack {
+                self.content
+            }
+            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: collapsed ? 0 : .none)
+            .clipped()
+            .animation(.easeIn)
+            .transition(.slide)
+            
+        }
+    }
+}
 
 #Preview {
     HomeView()
