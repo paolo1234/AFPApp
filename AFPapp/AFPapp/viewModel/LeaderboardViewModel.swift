5e916a7dd6e705b4986a02a579f55380acb6990d
Paolo Calamia <paolocalamia02@gmail.com>
2025-02-19 09:37:38 +0100

    Leaderboard position fix

--- AFPapp/AFPapp/viewModel/LeaderboardViewModel.swift
+++ AFPapp/AFPapp/viewModel/LeaderboardViewModel.swift
@@ -11,7 +11,7 @@ import FirebaseFirestore
 @MainActor
 class LeaderboardViewModel: ObservableObject {
     @Published var topUsers: [User] = []
-
+    
     func listenToTop3Users() {
         Firestore.firestore()
             .collection("users")
@@ -27,12 +27,11 @@ class LeaderboardViewModel: ObservableObject {
                 guard let documents = snapshot?.documents else { return }
                 
                 let fetchedUsers = documents.enumerated().compactMap { (index, document) -> User? in
-                    var user = try? document.data(as: User.self) // Creiamo un `var user`
+                    var user = try? document.data(as: User.self)
                     user?.posizione = index + 1  // Assegniamo la posizione
                     return user
                 }
                 
-                // Aggiorna la proprietà osservabile
                 self.topUsers = fetchedUsers
             }
     }
